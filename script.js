9f82a4a2e97ceaa3d211120e6538c3c5c8e76486
Yazdan <ykakaei.psy@gmail.com>
2018-10-17 22:15:35 +0200

    adding event listener

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 //This module takes care of the the data manipulation
-var budgetController = (function() {
-  var x = 23;
-  var add = function(a) {
-    return x + a;
-  };
-
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  };
-})();
+var budgetController = (function() {})();
 
 //This module takes care of the user interface
-
 var UIController = (function() {
   //Code body goes here
 })();
 
 //This module connects the two modules above
-
 var controller = (function(budgetCtrl, UICtrl) {
-  var z = budgetCtrl.publicTest(5);
-  return {
-    anotherPublic: function() {
-      console.log(z);
-    }
+  var ctrlAddItem = function() {
+    //1. Get the field input data
+    //2. Add the item to the budget controller
+    //3. Add the item to the UI
+    //4. Calculate the budget
+    //5.Display the budget
+    console.log("Something");
   };
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+
+  document.addEventListener("keyup", function(e) {
+    // console.log(e);
+    if (e.keyCode === 13 || e.which === 13) {
+      ctrlAddItem();
+    }
+  });
 })(budgetController, UIController);
