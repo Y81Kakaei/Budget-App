cec7ac734f06284873af198bab0aac6a84756c55
Yazdan <ykakaei.psy@gmail.com>
2018-10-23 14:27:09 +0200

    adding incomes and expenses function structures

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 //This module takes care of the the data manipulation
-var budgetController = (function() {})();
+var budgetController = (function() {
+  //Creating the data model for expenses
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  //Creating the data model for incomes
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+})();
 
 //This module takes care of the user interface
 var UIController = (function() {
